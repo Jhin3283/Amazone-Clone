8d2a95d1039cc24cdaa5a9e0c80fa6f7d35fa90a
Jhin3283 <hj3283@gmail.com>
2022-06-11 16:39:10 +0900

    Add

---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <div>
+      <div className="product">
+        <div className="product__info">
+          <p>Title</p>
+          <p className="product__price">
+            <small>$</small>
+            <strong>30</strong>
+          </p>
+          <div className="product__rating">⭐️⭐️⭐️⭐️⭐️</div>
+        </div>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+          alt="product__image"
+        />
+        <button>Add To Basket</button>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
