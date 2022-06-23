d535a3c26c20afcf8303f7c7114a29d756a4b259
Jhin3283 <hj3283@gmail.com>
2022-06-23 20:39:06 +0900

    Fixj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "Checkout.css";
+import "./Checkout.css";
 
 function Checkout() {
   return (
     <div>
-      <h1></h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
