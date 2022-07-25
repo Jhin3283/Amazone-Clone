1a4d63aaea3e39426666632419a3a3632d20bbec
Jhin3283 <hj3283@gmail.com>
2022-07-25 23:04:04 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import "./App.css";
-import Header from "./Header";
 import Home from "./Home";
+import Header from "./Header";
 import Checkout from "./Checkout";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Login from "./Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/checkout" element={<Checkout />}></Route>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route path="/login">{/* <Login /> */}</Route>
+
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
