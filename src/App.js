3f9faf60936e513db237eee511b472722fbf5e80
Jhin3283 <hj3283@gmail.com>
2022-08-21 22:36:50 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
 import Checkout from "./Checkout";
-// import Login from "./Login";
+import Login from "./Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -10,12 +10,15 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/login">{/* <Login /> */}</Route>
+          <Route path="/login">
+            <Login />
+          </Route>
 
           <Route path="/checkout">
             <Header />
             <Checkout />
           </Route>
+
           <Route path="/">
             <Header />
             <Home />
